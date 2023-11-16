03e13659935ad32715aaf7b9578b9c1a7aae96fe
Kate <mitsutanaka29@gmail.com>
2023-11-16 20:41:05 +0300

    added desktop details

--- src/component/Details.tsx
+++ src/component/Details.tsx
@@ -20,7 +20,7 @@ function Details({ count, setCount }: DetailsProps) {
     }
 
     return (
-        <section className="mt-5 pb-[88px]">
+        <section className="lg:w-[40%] mt-5 pb-[88px] lg:pb-0 lg:mt-0">
             <p className='font-xxs'>
                 Sneaker Company
             </p>
@@ -29,36 +29,38 @@ function Details({ count, setCount }: DetailsProps) {
                 Fall Limited Edition Sneakers
             </h2>
 
-            <p className="font-xs mt-4">
+            <p className="font-xs mt-4 lg:mt-9">
                 These low-profile sneakers are your perfect casual wear companion. Featuring a
                 durable rubber outer sole, they’ll withstand everything the weather can offer.
             </p>
 
-            <div className="mt-[30px] flex items-center">
+            <div className="mt-[30px] flex items-center flex-wrap lg:gap-2">
                 <ins className='font-xl'>$125.00</ins>
                 <span className='ml-4 badge'>50%</span>
-                <del className='font-m text-[#1D2025] font-bold ml-auto'>$250.00</del>
+                <del className='font-m text-[#1D2025] font-bold ml-auto tracking-wider lg:ml-0 lg:basis-full'>$250.00</del>
             </div>
 
-            <div className='flex justify-between items-center rounded-[10px] mt-[27px] px-6 py-4 bg-[#F7F8FD] font-m text-[#1D2025] font-bold'>
-                <button onClick={onMinusButtonClick} className='cursor-pointer'>
-                    <svg xmlns="http://www.w3.org/2000/svg" role='img' width='12' height="4">
-                        <use xlinkHref={`#${minusIcon}`} />
+            <div className="flex gap-4 flex-col lg:flex-row mt-[27px] lg:mt-[34px]">
+                <div className='lg:w-[36%] flex justify-between items-center rounded-[10px] px-6 py-4 bg-[#F7F8FD] font-m text-[#1D2025] font-bold'>
+                    <button onClick={onMinusButtonClick} className='cursor-pointer'>
+                        <svg xmlns="http://www.w3.org/2000/svg" role='img' width='12' height="4">
+                            <use xlinkHref={`#${minusIcon}`} />
+                        </svg>
+                    </button>
+                    <span>{count}</span>
+                    <button onClick={onPlusButtonClick} className='cursor-pointer'>
+                        <svg xmlns="http://www.w3.org/2000/svg" role='img' width='12' height="12">
+                            <use xlinkHref={`#${plusIcon}`} />
+                        </svg>
+                    </button>
+                </div>
+                <button className='lg:w-[63%] flex justify-center gap-4 align-center w-full rounded-[10px] py-4 bg-[#FF7D1A] font-m text-white font-bold lg:shadow-lg lg:shadow-orange-200'>
+                    <svg className="filter-white" width="20" height="20" xmlns="http://www.w3.org/2000/svg" role='img'>
+                        <use fill="#fff" xlinkHref={`#${cartIcon}`} />
                     </svg>
-                </button>
-                <span>{count}</span>
-                <button onClick={onPlusButtonClick} className='cursor-pointer'>
-                    <svg xmlns="http://www.w3.org/2000/svg" role='img' width='12' height="12">
-                        <use xlinkHref={`#${plusIcon}`} />
-                    </svg>
+                    <span>Add to cart</span>
                 </button>
             </div>
-            <button className='flex justify-center gap-4 align-center w-full rounded-[10px] py-4 mt-4 bg-[#FF7D1A] font-m text-white font-bold '>
-                <svg className="filter-white" width="20" height="20" xmlns="http://www.w3.org/2000/svg" role='img'>
-                    <use fill="#fff" xlinkHref={`#${cartIcon}`} />
-                </svg>
-                <span>Add to cart</span>
-            </button>
         </section>
     )
 }
--- tailwind.config.js
+++ tailwind.config.js
@@ -12,6 +12,9 @@ export default {
             borderColor: {
                 orange: '#FF7D1A'
             },
+            letterSpacing: {
+                wider: '.03em',
+            }
         },
     },
     plugins: [],
